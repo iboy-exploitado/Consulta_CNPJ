d405aaa302075701d1471823e3733b0a85028616
Matheus <76071892+iboy-exploitado@users.noreply.github.com>
2021-01-27 17:45:26 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import os
+import json
+import simplejson
+
+os.system("cls")
+
+print("""
+
+   ___                  _ _           ___ _  _ ___    _ 
+  / __|___ _ _  ____  _| | |_ __ _   / __| \| | _ \_ | |
+ | (__/ _ \ ' \(_-< || | |  _/ _` | | (__| .` |  _/ || |
+  \___\___/_||_/__/\_,_|_|\__\__,_|  \___|_|\_|_|  \__/ 
+                                                        
+Desenvolvido by: Iboy-Exploitado utilizando a API da receita federal.
+
+	""")
+cnpj = input("Insira o CNPJ: ")
+req = requests.get(f"https://www.receitaws.com.br/v1/cnpj/{cnpj}")
+
+json = req.json()
+
+for key, value in json.items():
+    print(f'{key}:, {value}:') 
+
+
+
